b471e18570c4e7cb7c2bcd15d5384f14d5dd55a7
Michal Dyczkowski <michal.dyczkowski@bbbit.io>
2024-04-23 08:02:59 +0200

    Hardcoded not rendering hidden tabs

--- src/view/Tab.tsx
+++ src/view/Tab.tsx
@@ -1,13 +1,14 @@
 import * as React from "react";
-import { Fragment } from "react";
+
 import { Actions } from "../model/Actions";
+import { BorderNode } from "../model/BorderNode";
+import { CLASSES } from "../Types";
+import { ErrorBoundary } from "./ErrorBoundary";
+import { Fragment } from "react";
+import { I18nLabel } from "../I18nLabel";
+import { ILayoutCallbacks } from "./Layout";
 import { TabNode } from "../model/TabNode";
 import { TabSetNode } from "../model/TabSetNode";
-import { CLASSES } from "../Types";
-import { ILayoutCallbacks } from "./Layout";
-import { ErrorBoundary } from "./ErrorBoundary";
-import { I18nLabel } from "../I18nLabel";
-import { BorderNode } from "../model/BorderNode";
 import { hideElement } from "./Utils";
 
 /** @internal */
@@ -29,8 +30,10 @@ export const Tab = (props: ITabProps) => {
             // load on demand
             // console.log("load on demand: " + node.getName());
             setRenderComponent(true);
+        } else if (renderComponent && !selected) {
+            setRenderComponent(false);
         }
-    });
+    }, [selected, renderComponent, setRenderComponent]);
 
     const onMouseDown = () => {
         const parent = node.getParent() as TabSetNode;
@@ -66,23 +69,16 @@ export const Tab = (props: ITabProps) => {
         className += " " + cm(CLASSES.FLEXLAYOUT__TAB_BORDER);
         className += " " + cm(CLASSES.FLEXLAYOUT__TAB_BORDER_ + parentNode.getLocation().getName());
     }
-    
+
     if (node.getContentClassName() !== undefined) {
         className += " " + node.getContentClassName();
     }
 
     return (
-        <div
-            className={className}
-            data-layout-path={path}
-            onMouseDown={onMouseDown}
-            onTouchStart={onMouseDown}
-            style={style}>
+        <div className={className} data-layout-path={path} onMouseDown={onMouseDown} onTouchStart={onMouseDown} style={style}>
             <ErrorBoundary message={props.layout.i18nName(I18nLabel.Error_rendering_component)}>
                 <Fragment>{child}</Fragment>
             </ErrorBoundary>
         </div>
     );
 };
-
-
